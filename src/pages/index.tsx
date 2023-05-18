1120762597bdb475bb270bb04204e4a64436f167
jhlee <jhlee@spicusinc.com>
2023-05-18 19:00:10 +0900

    edit service

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   const { data: session, status } = useSession();
   const FacebookHandler = () => {
-    console.log(session);
+    console.log(session.user);
     if (session) {
       signOut();
     } else {
